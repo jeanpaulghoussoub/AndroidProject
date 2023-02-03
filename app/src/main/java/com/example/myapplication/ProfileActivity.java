d42eaaf28a61e8e4e2f5ade51d8178b416c58de4
Jean-Paul Ghoussoub <jeanpaulghoussoub@gmail.com>
2023-02-03 14:27:48 +0200

    removed line

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -10,8 +10,6 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 }
